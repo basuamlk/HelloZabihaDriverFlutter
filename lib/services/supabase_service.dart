3d3bd3a71fd1a977c0b6b020e74dcafd73cd12ee
Basu Woldatsadik <basu@Basus-MacBook-Pro.local>
2026-02-01 23:11:20 -0500

    Fix Supabase API key

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -3,7 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class SupabaseService {
   static const String supabaseUrl = 'https://xdatjvzwjevqqugynkyf.supabase.co';
   static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkYXRqdnp3amV2cXF1Z3lua3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNjk2MjcsImV4cCI6MjA2NDY0NTYyN30.cP4yrMPFVfLLQo6J4ZisfD-hSzBxCD3bhur1eSSmDao';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkYXRqdnp3amV2cXF1Z3lua3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2Nzg0NTEsImV4cCI6MjA4MjI1NDQ1MX0.RKGGAqHjxzJR3T2TPldomGJGBdhbAB9DEBJT9WlPIPg';
 
   static SupabaseClient get client => Supabase.instance.client;
 
